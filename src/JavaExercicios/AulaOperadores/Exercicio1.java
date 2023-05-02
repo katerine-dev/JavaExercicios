bf88e9e4cfd370c132fc702d3dc02160f33f9eeb
Katerine Witkoski <ka.lwitkoski@gmail.com>
2023-05-02 15:09:59 -0300

    Adiciona arquivos construtores do projeto e realiza o primeiro exercicio

---
+++ src/JavaExercicios/AulaOperadores/Exercicio1.java
@@ -0,0 +1,21 @@
+package JavaExercicios.AulaOperadores;
+
+/**
+ * @author katerinelindawitkoski
+ */
+
+/*
+Faça um algoritmo para imprimir em cada linha:
+- o seu nome completo
+- o seu RA
+- o seu curso
+ */
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Katerine Witkoski");
+        System.out.println("12345678910");
+        System.out.println("Ciência da Computação");
+    }
+    
+}
