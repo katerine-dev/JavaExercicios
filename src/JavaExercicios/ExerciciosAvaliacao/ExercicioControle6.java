ac9fa530db7a5fd22302b7bb4fc35cc765baf201
Katerine Witkoski <ka.lwitkoski@gmail.com>
2023-05-09 11:55:09 -0300

    Exercicio 6 de controle do Bruno

---
+++ src/JavaExercicios/ExerciciosAvaliacao/ExercicioControle6.java
@@ -0,0 +1,39 @@
+package JavaExercicios.ExerciciosAvaliacao;
+
+/**
+ * @author katerinelindawitkoski
+ */
+
+/*
+Escreva um aplicativo que utiliza apenas as instruções de saída
+System.out.print("* ");
+System.out.print(" ");
+System.out.println();
+para exibir o padrão de tabuleiro de damas a seguir:
+
+* * * * * * * *
+ * * * * * * * *
+* * * * * * * *
+ * * * * * * * *
+* * * * * * * *
+ * * * * * * * *
+* * * * * * * *
+ * * * * * * * *
+ */
+
+public class ExercicioControle6 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.print(
+                "* * * * * * * *\n" +
+                " * * * * * * * *\n" +
+                "* * * * * * * *\n" +
+                " * * * * * * * *\n" +
+                "* * * * * * * *\n" +
+                " * * * * * * * *\n" +
+                "* * * * * * * *\n" +
+                " * * * * * * * *");
+        System.out.println();
+    }
+
+}
