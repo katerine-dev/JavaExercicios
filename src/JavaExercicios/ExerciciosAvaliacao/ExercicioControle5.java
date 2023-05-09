b4b8b44ec9130c7fb5655659bd4a10b602e770f1
Katerine Witkoski <ka.lwitkoski@gmail.com>
2023-05-09 11:52:05 -0300

    Exercicio 5 de controle do Bruno

--- src/JavaExercicios/ExerciciosAvaliacao/ExercicioControle5.java
+++ src/JavaExercicios/ExerciciosAvaliacao/ExercicioControle5.java
@@ -14,7 +14,36 @@ Seu programa deve trabalhar com quadrados de todos os comprimentos de lado poss√
 
 public class ExercicioControle5 {
     public static void main(String[] args) {
-        
+        Scanner entrada = new Scanner(System.in);
+        int ladoQuadrado = 0;
+        String mensagem = "Insira o tamanho do lado do quadrado [comprimentos entre 1 e 20]: ";
+
+
+        System.out.println(mensagem);
+        ladoQuadrado = entrada.nextInt();
+        if (ladoQuadrado < 1 || ladoQuadrado > 20) {
+            System.out.println("Tamanho de comprimento incorreto");
+            System.out.println();
+            System.out.print(mensagem);
+            ladoQuadrado = entrada.nextInt();
+        }
+
+        char[][] quadrado = new char[ladoQuadrado][ladoQuadrado];
+        for (int linha = 0; linha < ladoQuadrado; linha++) {
+            for (int coluna = 0; coluna < ladoQuadrado; coluna++) {
+                if (linha == 0 || linha == ladoQuadrado - 1 || coluna == 0 || coluna == ladoQuadrado - 1) {
+                    quadrado[linha][coluna] = '*';
+                } else {
+                    quadrado[linha][coluna] = ' ';
+                }
+            }
+            System.out.println();
+            for (int i = 0; i < quadrado[linha].length; i++){
+                System.out.print(quadrado[linha][i] + " ");
+
+            }
+        }
+        entrada.close();
 
     }
 
